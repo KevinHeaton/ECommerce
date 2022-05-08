a2ff23686b69d9150179c664d7723e0a40dd0cbf
Kevin Heaton <kevin.heaton21@yahoo.com>
2022-05-08 09:30:22 -0500

    completed tag and product routes

--- Develop/db/seed.sql
+++ Develop/db/seed.sql
@@ -1,9 +1,19 @@
--- CREATE TABLE category (
--- )
+INSERT INTO category (category_name)
+VALUES
+('Tops'),
+('Bottoms'),
+('Hats');
 
--- INSERT INTO category (category_name)
--- VALUES
--- ('Shirts'),
--- ('Pants'),
--- ('Shorts'),
--- ('Hats');
+INSERT INTO product (product_name, price, stock, category_id)
+VALUES
+('Button Down', '29.99', 20, 1),
+('Classic T', '12.99', 15, 1),
+('Jeans', '49.99', 36, 2),
+('Shorts', '24.99', 25, 2),
+('Baseball Cap', '29.99', 30, 3),
+('Cowboy', '50.00', 6, 3);
+
+INSERT INTO tag (tag_name)
+VALUES
+('Dress'),
+('Casual');
